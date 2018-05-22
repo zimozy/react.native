5ec158a0439af1ab2756959878762d6104e83ff6
Tim <eafwas@gmail.com>
2018-05-21 21:04:45 -0400

    first commit

---
+++ Awesome/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <View style={styles.container}>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Text>Hello, world!</Text>
+      </View>
+      
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
